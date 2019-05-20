66fad7c575c2d91555b9f460cca920fcbb0b36c8
Zakro Khitalishvili <zakro1996@gmail.com>
2019-05-20 19:49:06 +0400

    fixes

--- Application/DTOs/Answers/TextAnswerDTO.cs
+++ Application/DTOs/Answers/TextAnswerDTO.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Application.DTOs
 {
     public class TextAnswerDTO
     {
+        [Required]
         public string Text { get; set; }
 
         public bool IsIssue { get; set; }
