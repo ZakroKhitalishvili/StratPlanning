659ca575ca5ae61676eb0c8dd778b1bbc0590d13
Levan Lobjanidze <levalobjanidze@gmail.com>
2019-05-24 17:04:00 +0400

    dictionary_hasStakeholderCategory

---
+++ Core/Migrations/20190524130257_Dictionary_hasStakeholderCategory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Migrations
+{
+    public partial class Dictionary_hasStakeholderCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasStakeholderCategory",
+                table: "Dictionaries",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasStakeholderCategory",
+                table: "Dictionaries");
+        }
+    }
+}
